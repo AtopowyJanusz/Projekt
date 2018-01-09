df589286eb051de56c802d5ec8d34e3b81b53d3a
AtopowyJanusz <patrycja.bednarska@gmail.com>
2018-01-09 07:50:09 +0100

    Dodanie funkcji menu

--- Projekt/Projekt.cpp
+++ Projekt/Projekt.cpp
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+void menu()
+{
+    int znakAscii, szerokoscZnaku, wysokoscZnaku;
+ cout<<"Witam w programie rysowania znakami ASCII :)"<<endl;
+ cout<<"Najpierw kilka zasad: "<<endl;
+ cout<<"*Mozesz przesuwaæ figure strzalkami(lewo,prawo,gora,dol)"<<endl;
+ cout<<"*Mozesz pomniejszac lub powiekszac firuge znakiem + lub -"<<endl;
+ cout<<"UWAGA! figura bedzie poruszala sie tylko w obszarze twojego ekranu"<<endl;
+ cout<<"Zacznijmy zabawe!"<<endl;
+ cout<<"Najpierw podaj znak ASCII, ktorym bedziemy rysowac"<<endl;
+ cin>>znakAscii;
+ cout<<"Teraz podaj wymiar poczatkowy znaku "<<endl;
+ cout<<"Szerokosc:";
+ cin>>szerokoscZnaku;
+ cout<<"Wysokosc:";
+ cin>>wysokoscZnaku;
+ cout<<endl; 
+}
+
 int main()
 {
+    menu();
     getchar();
     return EXIT_SUCCESS;
 }
