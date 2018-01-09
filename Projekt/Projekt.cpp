af54df2d1be4dab4623b5dd5ea5952e8e4c53a82
AtopowyJanusz <patrycja.bednarska@gmail.com>
2018-01-09 11:33:48 +0100

    Pobranie srodka ekranu

--- Projekt/Projekt.cpp
+++ Projekt/Projekt.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 
+// Podczas pisania aplikacji korzystalam miedzy innymi z pomocy z nastepujacych stron internetowych:
+// http://www.cplusplus.com/forum/general/58945/
+
 char znakAscii;
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
 void wypiszZasady()
 {
-    
     cout<<"Witam w programie rysowania znakami ASCII :)"<<endl;
     cout<<"Najpierw kilka zasad: "<<endl;
     cout<<"*Mozesz przesuwaæ figure strzalkami(lewo,prawo,gora,dol)"<<endl;
     cout<<"*Mozesz pomniejszac lub powiekszac firuge znakiem + lub -"<<endl;
     cout<<"UWAGA! figura bedzie poruszala sie tylko w obszarze twojego ekranu"<<endl;
-    cout<<"Zacznijmy zabawe!"<<endl;
-     
+    cout<<"Zacznijmy zabawe!"<<endl;    
 }
-
+    
 int main()
 {
-    int szerokoscZnaku, wysokoscZnaku;
+    int rozmiarFigury,
+        szerokoscEkranu,
+        wysokoscEkranu
+        ;
+
+    // Pobranie szerokosci i wysokosci ekranu
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(console,&csbi);
+    szerokoscEkranu = csbi.dwSize.X;
+    wysokoscEkranu = csbi.dwSize.Y;
+
     wypiszZasady();
-    cout<<"Najpierw podaj znak ASCII, ktorym bedziemy rysowac"<<endl;
-    cin>>znakAscii;
-    cout<<"Teraz podaj wymiar poczatkowy znaku "<<endl;
-    cout<<"Szerokosc:";
-    cin>>szerokoscZnaku;
-    cout<<"Wysokosc:";
-    cin>>wysokoscZnaku;
-    cout<<endl; 
-    cout<<endl;
-    cout<<endl;
-    cout<<wysokoscZnaku<<endl;
-    cout<<szerokoscZnaku<<endl;
-    cout<<znakAscii<<endl;
 
+    cout << "Najpierw podaj znak ASCII, ktorym bedziemy rysowac" << endl;
+    cin >> znakAscii;
+    cout << "Teraz podaj poczatkowy rozmiar figury : ";
+    cin >> rozmiarFigury;
 
+    system("cls");
+    
     getch();
     return EXIT_SUCCESS;
 }
